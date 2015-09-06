b9c0cc83d55e5b1148e10aa7f20670b58b521bba
Tim Prince <tim.prince@gmail.com>
2015-09-05 17:40:55 -0700

    Update submodules.

--- init.sh
+++ init.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e
+sudo apt-get install vim-gnome build-essential cmake
 git submodule update --init --recursive
 if [ ! -e ${HOME}/.vimrc ]; then
   ln -s ${HOME}/.vim/vimrc ${HOME}/.vimrc
