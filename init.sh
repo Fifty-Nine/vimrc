d7eec4e792e24c3eba3f092578c2bf6238548c5e
Tim Prince <tim.prince@gmail.com>
2024-10-29 19:33:49 -0700

    Make init script docker-friendly.

--- init.sh
+++ init.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 set -e
-sudo apt-get install vim-nox build-essential cmake
+
+if [ "$EUID" -ne 0 ]; then
+  sudo apt-get install vim-nox build-essential cmake
+else
+  apt-get install vim-nox build-essential cmake
+fi
 git submodule update --init --recursive
 if [ ! -e ${HOME}/.vimrc ]; then
   ln -s ${HOME}/.vim/vimrc ${HOME}/.vimrc
 fi
-vim -c 'PluginInstall'
+ex -c 'PluginInstall' -c 'qall'
